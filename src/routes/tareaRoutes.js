4a016327192d44fc42bd7d7c3e49f193d4fae25f
Cristofer <c.luciano031211@gmail.com>
2026-07-28 20:55:23 -0400

    feat: endpoints para listar y consultar tareas

--- src/routes/tareaRoutes.js
+++ src/routes/tareaRoutes.js
@@ -3,5 +3,7 @@ const router = express.Router();
 const tareaController = require('../controllers/tareaController');
 
 router.post('/', tareaController.crear);
+router.get('/', tareaController.listar);
+router.get('/:id', tareaController.obtenerPorId);
 
 module.exports = router;
